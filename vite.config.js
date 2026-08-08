e4b77480a0e90f2cd057fb9a7596e42efb8dc94a
Kufre Obot <kufreobot08@gmail.com>
2026-08-08 20:47:02 +0100

    ggg

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/zynx-movies/',
+  base: '/Zynx_movies/',
   server: {
     port: 5173,
     open: true,
